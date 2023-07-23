c7aab66868a4d3e2e81fbd04f3e657e814e1bf04
Vo Van Viet <vovanviet4444@gmail.com>
2023-07-23 23:54:19 +0700

    More UI Added

--- WebCopy.js
+++ WebCopy.js
@@ -20,21 +20,42 @@ class BCS{
 /**@type {BCS[]} */
 
 
+
 function buildNavbar() {
   let container = document.createElement("div");
   container.className = "NavBar";
+
+  let Leftcontainer = document.createElement("div");
+  container.appendChild(Leftcontainer);
+  let Rightcontainer = document.createElement("div");
+  container.appendChild(Rightcontainer);
+
+  let Logo = document.createElement("div");
+  Logo.innerHTML="<img src='Logo.png'>";
+  Leftcontainer.appendChild(Logo);
 
   let AddToCartButton = document.createElement("button");
   AddToCartButton.innerHTML = `<h1 style = "padding: 20px">SHOW CART</h1>`;
   AddToCartButton.className = "NavButton";
   AddToCartButton.addEventListener("click", () => {ShowCart();})
-  container.appendChild(AddToCartButton);
+  Rightcontainer.appendChild(AddToCartButton);
 
   let CheckOutButton = document.createElement("button");
   CheckOutButton.innerHTML = `<h1 style = "padding: 20px">CHECK OUT</h1>`;
   CheckOutButton.className = "NavButton";
   CheckOutButton.addEventListener("click", () => {CheckOut();})
-  container.appendChild(CheckOutButton);
+  Rightcontainer.appendChild(CheckOutButton);
+
+  return container;
+}
+
+function buildHeader(){
+  let container = document.createElement("div");
+  
+  let Background = document.createElement("div");
+  Background.innerHTML = "<img src = 'Background.png' style = 'width: 99vw; box-sizing:border-box'>";
+  container.appendChild(Background);
+  
   return container;
 }
 
@@ -99,17 +120,16 @@ function buildSidebar() {
   container.className = "sideBar";
 
   let Header = document.createElement("div");
-  Header.innerHTML = "<h1>21 SẢN PHẨM</h1>";
+  Header.innerHTML = "<h1 style='color: #005baa;'>21 SẢN PHẨM</h1>";
   Header.style.textAlign = "center";
   container.appendChild(Header);
 
   let PackSize = document.createElement("div");
   PackSize.className = "PackSizeBar";
-
   container.appendChild(PackSize);
 
   let PackSizeHeader = document.createElement("div");
-  PackSizeHeader.innerHTML=`<h2 style = "margin: 20px 80px 10px">PACK SIZE</h3>`
+  PackSizeHeader.innerHTML=`<h2 style = "margin: 20px 80px;color:gray;">PACK SIZE</h2>`
   PackSize.appendChild(PackSizeHeader);
 
   let PackSizeButtonContainer = document.createElement("div");
@@ -117,15 +137,22 @@ function buildSidebar() {
   PackSizeButtonContainer.style.display="flex";
   PackSize.appendChild(PackSizeButtonContainer);
 
-  let PackSizeArr = [3,5,10];
+  let PackSizeArr = [3,10,12];
 
   for (let i = 0; i < PackSizeArr.length; i++) {
     let PackSizeButton = document.createElement("button");
     PackSizeButton.className="PackSizeButton";
-    PackSizeButton.innerHTML = `<h1>${PackSizeArr[i]}</h1>`;
+    PackSizeButton.innerHTML = `<p>${PackSizeArr[i]}</p>`;
     PackSizeButtonContainer.appendChild(PackSizeButton);
   }
   container.appendChild(PackSize);
+
+  let ListContainer = document.createElement("div");
+  ListContainer.style.display="flex";
+  ListContainer.style.justifyContent="center";
+  ListContainer.style.margin="30px 0";
+  ListContainer.innerHTML="<img src='List.png'>";
+  container.appendChild(ListContainer);
 
   return container;
 }
@@ -137,7 +164,7 @@ function buildItemsMenu() {
   for (let i = 0; i < myJson.length; i++) {
     let card = document.createElement("div");
     card.className = "card";
-    card.innerHTML = `<img src= "${myJson[i].image}" style="margin: 30px 0; width: 180px;height: 180px;">
+    card.innerHTML = `<img src= "${myJson[i].image}" style="margin: 30px 0; width: 370px;height: 370px;">
       <h1 style="margin: 0 0 4px 0; font-size: 30px; color: black;">${myJson[i].name}</h1>
       <h2 style="margin: 0 0 4px 0; font-size: 30px; color: black;">${myJson[i].price} đồng</h2>`;
     container.appendChild(card);
@@ -145,7 +172,7 @@ function buildItemsMenu() {
     let bcs= myJson[i];
 
     let AddToCartButton = document.createElement("button");
-    AddToCartButton.className = "NavButton";
+    AddToCartButton.className = "cardButton";
     AddToCartButton.innerHTML = `<h1 style = "padding: 20px;">ADD TO CART</h1>`;
     AddToCartButton.addEventListener("click", () => {
       addToCart(bcs);
@@ -155,11 +182,25 @@ function buildItemsMenu() {
   return container;
 }
 
+function buildFooter(){
+  let container = document.createElement("div");
+  
+  let Background = document.createElement("div");
+  Background.innerHTML = "<img src = 'Footer.png' style = 'width: 99vw; box-sizing:border-box'>";
+  container.appendChild(Background);
+  
+  return container;
+}
+
 let newNavBar = buildNavbar();
 body.appendChild(newNavBar);
+let newHeader = buildHeader();
+body.appendChild(newHeader);
 let newBody = buildBody();
 body.appendChild(newBody);
 let newSideBar = buildSidebar();
 newBody.appendChild(newSideBar);
 let newcontainer = buildItemsMenu();
 newBody.appendChild(newcontainer);
+let newFooter = buildFooter();
+body.appendChild(newFooter);
